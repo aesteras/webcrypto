8829157249bc48cc8c73cd62b9b250682d529ff5
Andreu Esteras <andreu@andreuesteras.com>
2018-12-13 13:19:21 +0100

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync('ssl/key.pem'),
+    cert: fs.readFileSync('ssl/cert.pem')
+};
+
+const app = express();
+app.use(express.static(path.join(__dirname, '../static')));
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../static'));
+});
+
+const server = https.createServer(options, app);
+const port = 443;
+server.listen(port);
+server.on('listening', () => {
+    console.log('Server listening on port ' + port + '.');
+});
